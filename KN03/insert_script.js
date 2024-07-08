ee1ea583b31c2d27bc92da3208bf02117dfe36e1
plug <112622227+duyminh-nguyen@users.noreply.github.com>
2024-07-08 08:37:19 +0200

    Update insert_script.js

--- KN03/insert_script.js
+++ KN03/insert_script.js
@@ -1,4 +1,4 @@
-// This Defines ObjectIds for characters, games, and weapons
+// This Defines ObjectIds for characters, games, weapons, and developers
 var cloudId = new ObjectId();
 var aerithId = new ObjectId();
 var lightningId = new ObjectId();
@@ -14,7 +14,13 @@ var brotherhoodId = new ObjectId();
 var ff7Id = new ObjectId();
 var ff13Id = new ObjectId();
 var ff15Id = new ObjectId();
-var ff10Id = new ObjectId(); 
+var ff10Id = new ObjectId();
+
+var developer1Id = new ObjectId();
+var developer2Id = new ObjectId();
+var developer3Id = new ObjectId();
+var developer4Id = new ObjectId();
+var developer5Id = new ObjectId();
 
 // This Inserts weapons into the 'weapon' collection
 db.weapon.insertMany([
@@ -45,41 +51,55 @@ db.weapon.insertMany([
     }
 ]);
 
-// This Inserts games into the 'game' collection
+// This Inserts games into the 'game' collection, including developers
 db.game.insertMany([
     {
         _id: ff7Id,
         Game_ID: 1,
         Title: "Final Fantasy VII",
-        Release_Year: ISODate("1997-01-31")
+        Release_Year: ISODate("1997-01-31"),
+        Developers: [
+            { _id: developer1Id, Name: "Square" }
+        ]
     },
     {
         _id: ff13Id,
         Game_ID: 2,
         Title: "Final Fantasy XIII",
-        Release_Year: ISODate("2009-12-17")
+        Release_Year: ISODate("2009-12-17"),
+        Developers: [
+            { _id: developer2Id, Name: "Square Enix" }
+        ]
     },
     {
         _id: ff15Id,
         Game_ID: 3,
         Title: "Final Fantasy XV",
-        Release_Year: ISODate("2016-11-29")
+        Release_Year: ISODate("2016-11-29"),
+        Developers: [
+            { _id: developer3Id, Name: "Square Enix" }
+        ]
     },
     {
         _id: ff10Id,
         Game_ID: 4,
         Title: "Final Fantasy X",
-        Release_Year: ISODate("2001-07-19")
+        Release_Year: ISODate("2001-07-19"),
+        Developers: [
+            { _id: developer4Id, Name: "Square" },
+            { _id: developer5Id, Name: "Square Enix" }
+        ]
     }
 ]);
 
-// This Inserts characters into the 'character' collection using insertOne
+// This Inserts characters into the 'character' collection, including main_weapon
 db.character.insertOne({
     _id: cloudId,
     First_Name: "Cloud",
     Last_Name: "",
     Species: "Human",
-    Game: ff7Id
+    Game: ff7Id,
+    main_weapon: busterSwordId
 });
 
 db.character.insertOne({
@@ -87,7 +107,8 @@ db.character.insertOne({
     First_Name: "Aerith",
     Last_Name: "",
     Species: "Cetra",
-    Game: ff7Id
+    Game: ff7Id,
+    main_weapon: staffId
 });
 
 db.character.insertOne({
@@ -95,7 +116,8 @@ db.character.insertOne({
     First_Name: "Lightning",
     Last_Name: "Farron",
     Species: "Human",
-    Game: ff13Id
+    Game: ff13Id,
+    main_weapon: gunbladeId
 });
 
 db.character.insertOne({
@@ -103,7 +125,8 @@ db.character.insertOne({
     First_Name: "Noctis",
     Last_Name: "Lucis Caelum",
     Species: "Human",
-    Game: ff15Id
+    Game: ff15Id,
+    main_weapon: engineBladeId
 });
 
 db.character.insertOne({
@@ -111,12 +134,6 @@ db.character.insertOne({
     First_Name: "Tidus",
     Last_Name: "",
     Species: "Human",
-    Game: ff10Id
+    Game: ff10Id,
+    main_weapon: brotherhoodId
 });
-
-// This Links characters to their main weapons
-db.character.updateOne({ _id: cloudId }, { $set: { main_weapon: [busterSwordId] } });
-db.character.updateOne({ _id: aerithId }, { $set: { main_weapon: [staffId] } });
-db.character.updateOne({ _id: lightningId }, { $set: { main_weapon: [gunbladeId] } });
-db.character.updateOne({ _id: noctisId }, { $set: { main_weapon: [engineBladeId] } });
-db.character.updateOne({ _id: tidusId }, { $set: { main_weapon: [brotherhoodId] } });
